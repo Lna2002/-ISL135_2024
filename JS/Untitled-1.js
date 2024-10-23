1f5703fbe0c008b3eb52d783a49bc9857d96432e
Maru-chan503 <VT21004.EDU.SV>
2024-10-22 19:19:35 -0600

    Archivo JS

---
+++ JS/Untitled-1.js
@@ -0,0 +1,4 @@
+document.getElementById('cambiarTexto').addEventListener('click', function() {
+    const descripcion = document.getElementById('descripcion');
+    descripcion.textContent = '¡Has cambiado exitosamente el contenido de este párrafo!';
+});
