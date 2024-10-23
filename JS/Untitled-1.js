e0ded3164d2518f0ead47dd161c94257c1786da7
Elena <cg22079@ues.edu.sv>
2024-10-22 20:06:24 -0600

    Ajustes finales

--- JS/Untitled-1.js
+++
@@ -1,4 +0,0 @@
-document.getElementById('cambiarTexto').addEventListener('click', function() {
-    const descripcion = document.getElementById('descripcion');
-    descripcion.textContent = '¡Has cambiado exitosamente el contenido de este párrafo!';
-});
